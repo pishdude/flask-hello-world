f8f6d798b4d5d3ac91adbc4e745fccca3f8283a2
pish <hariarun.19@gmail.com>
2023-03-23 08:05:01 -0400

    test

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask,request
+import json
+import pymongo
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.2jnewlw.mongodb.net/?retryWrites=true&w=majority")
+db = client.tut7 #Choose the DB
+
+@app.route('/getMethod', methods=['GET'])
+def getMethod():
+    userCollection = db.Users                                           #choose our collection
+    results = userCollection.find({},{'_id':0})                         #execute our query
+    listOfUsers = list(results)                                         #convert cursor to list 
+    jsonList = json.dumps(listOfUsers)                                  #Convert to Json format 
+    return jsonList  
